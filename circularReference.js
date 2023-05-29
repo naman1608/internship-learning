be3f70ac98ccb94f956766980ab56e8592796f27
Naman Dhingra <74304916+naman1608@users.noreply.github.com>
2023-05-29 16:27:23 +0530

    circular reference with >2 objects

---
+++ circularReference.js
@@ -0,0 +1,20 @@
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Conference",
+  participants: ["john", "ann"]
+};
+
+let time = {
+  hour: 13,
+  minute: 30,
+  second: 20,
+};
+
+room.place = meetup;
+meetup.time = time;
+time.venue = room;
+
+console.log(JSON.stringify(meetup));
